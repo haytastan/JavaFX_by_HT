a0de30a2ab65fe92ce86e9e252c9d2b16ed2023c
hayyam <haytastan@gmail.com>
2022-09-28 13:51:03 +0300

    version 1.2

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +15,21 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 620, 240);
+
+        //Creating a scene object
+        Scene scene = new Scene(fxmlLoader.load(), 350, 500);
+
+
+        //Setting title to the Stage
         stage.setTitle("My First JavaFX Application");
+
+        //Adding scene to the stage
         stage.setScene(scene);
+
+        //Displaying the contents of the stage
         stage.show();
     }
 
